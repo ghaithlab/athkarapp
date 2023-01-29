91b13e1a974d9eaa2ffa03eaba2b92572d01dfe0
Ghaith <eng.ghaith.rushd@gmail.com>
2023-01-29 14:31:07 +0300

    clean version before the new stransition into habits app

---
+++ lib/homeScreen.dart
@@ -0,0 +1,22 @@
+import 'package:athkarapp/clicksPerDay.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import "package:flutter/material.dart";
+
+import 'heatMapWidget/data/heatmap_color_mode.dart';
+import 'heatMapWidget/heatmap.dart';
+
+import 'package:flutter/animation.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
